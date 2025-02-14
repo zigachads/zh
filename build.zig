44b7ca0a7290b6b7d5dc363a578115abe2093e96
Sped0n <hi@sped0n.com>
2025-02-15 00:45:01 +0800

    build: improve zls build process
    - Add a new executable for zls build_on_save
    - Create a separate step for checking if the executable compiles
    - Reorganize the build script to include separate steps for running and testing the application
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- build.zig
+++ build.zig
@@ -11,26 +11,36 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
-    {
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
+    // For zls build_on_save
+    const exe_check = b.addExecutable(.{
+        .name = "foo",
+        .root_source_file = b.path("src/main.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+
+    // Run
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
 
-    {
-        const test_module = b.createModule(.{ .root_source_file = b.path("src/test/tests.zig") });
-        const tests = b.addTest(.{
-            .name = "tests",
-            .root_source_file = b.path("src/main.zig"),
-        });
-        tests.root_module.addImport("tests", test_module);
-        const test_cmd = b.addRunArtifact(tests);
-        test_cmd.step.dependOn(b.getInstallStep());
-        const test_step = b.step("test", "Run the tests");
-        test_step.dependOn(&test_cmd.step);
-    }
+    // Test
+    const test_module = b.createModule(.{ .root_source_file = b.path("src/test/tests.zig") });
+    const tests = b.addTest(.{
+        .name = "tests",
+        .root_source_file = b.path("src/main.zig"),
+    });
+    tests.root_module.addImport("tests", test_module);
+    const test_cmd = b.addRunArtifact(tests);
+    test_cmd.step.dependOn(b.getInstallStep());
+    const test_step = b.step("test", "Run the tests");
+    test_step.dependOn(&test_cmd.step);
+
+    // For zls build_on_save
+    const check = b.step("check", "Check if foo compiles");
+    check.dependOn(&exe_check.step);
 }
